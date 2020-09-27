e496553e61b424e8f507491f6f8510a754fd65c1
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-09-27 00:41:55 -0500

    Gestion
    Atributos paquete gestion

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,5 +1,12 @@
 package gestion;
 
 public class Zona {
+	
+	
+	public void suma(int a) {
+		int q;
+		q= a+3;
+		System.out.println(q);
+	}
 
 }
--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -5,7 +5,9 @@ public class Zoologico {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola");
-
+		Zona xl = new Zona();
+		xl.suma(1);
+		
 	}
 
 }

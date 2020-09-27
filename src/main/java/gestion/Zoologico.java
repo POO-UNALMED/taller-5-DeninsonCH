3f271a60fb8c6511d6872c2ae21ae160edba649b
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-09-27 00:44:19 -0500

    gestion

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,12 +1,12 @@
 package gestion;
 
 public class Zoologico {
+	private String nombre;
+	private String ubicacion;
+	private Zona zonas = new Zona();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola");
-		Zona xl = new Zona();
-		xl.suma(1);
 		
 	}
 
--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -1,5 +1,11 @@
 package zooAnimales;
+import gestion.*;		/*me permite la conexion entre las clases de distintos paquetes*/
 
 public class Animal {
-
+	private int totalAnimales;
+	private String nombre;
+	private int edad;
+	private String habitat;
+	private String genero;
+	private Zona zona = new Zona();
 }

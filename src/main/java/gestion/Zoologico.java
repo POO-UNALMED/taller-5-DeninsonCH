151231899793fd287dd4fbafdbf3091e5d9c333f
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-09-29 01:47:07 -0500

    z

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,48 +1,48 @@
 package gestion;
 import java.util.*;
-
 public class Zoologico {
+	
+	private ArrayList<Zona>zona=new ArrayList<Zona>();
 	private String nombre;
 	private String ubicacion;
-	private ArrayList<Zona> zona = new ArrayList<Zona>();
-
-	public Zoologico(){}
-	public Zoologico(String nombre, String ubicacion) {
+	
+	
+	public Zoologico(String nombre,String ubicacion){
 		this.nombre=nombre;
 		this.ubicacion=ubicacion;
 	}
-	
-	
-	public void agregarZonas(Zona Z) {
-		zona.add(Z);
+	public Zoologico() {
+		
 	}
-	
-	public int cantidaTotalAnimales() {
-		int contador = 0;
-		for (int i = 0; i < zona.size(); i ++) {
-			contador += zona.get(i).cantidadAnimales();
+	public void agregarZonas(Zona zona1) {
+		zona.add(zona1);
+	}
+	public int cantidadTotalAnimales() {
+		int cont=0;
+		for (int i = 0; i < zona.size(); i++) {
+			cont=cont+zona.get(i).cantidadAnimales();
 		}
-		return(contador);
+		return cont;
+		
+		
 	}
-	/*metodos get*/
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
 	public ArrayList<Zona> getZona() {
-		return (this.zona);
+		return zona;
+	}
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zona = zonas;
 	}
 	public String getNombre() {
-		return (this.nombre);
+		return nombre;
 	}
-	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	public String getUbicacion() {
 		return ubicacion;
 	}
-	public void setUbicacion(String ubicacion) {
-		this.ubicacion = ubicacion;
-	}
 	
-	public void setZonas(ArrayList<Zona> zonas) {
-		this.zona = zonas;
-	}
 }

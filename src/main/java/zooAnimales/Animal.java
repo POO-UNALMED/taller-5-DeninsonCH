127b93f46b9593d262f0e628786929eb516d4534
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-09-29 02:12:55 -0500

    z

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -1,5 +1,6 @@
 package zooAnimales;
-import gestion.*;
+import gestion.*;		/*me permite la conexion entre las clases de distintos paquetes*/
+
 public class Animal {
 	private static int totalAnimales;
 	private String nombre;
@@ -8,71 +9,96 @@ public class Animal {
 	private String genero;
 	private Zona zona;
 	
-	public Animal(){
-		totalAnimales++;
-	}
-	public Animal(String nombre,int edad,String habitat,String genero){
-		totalAnimales++;
+	/*atributos por tipo*/
+	public static int tMamiferos;
+	public static int tAves;
+	public static int tReptiles;
+	public static int tPeces;
+	public static int tAnfibios;
+	
+	/*contructores*/
+	public Animal() {totalAnimales++;}
+	public Animal(String nombre, int edad, String habitat, String genero) {
 		this.nombre=nombre;
 		this.edad=edad;
 		this.habitat=habitat;
 		this.genero=genero;
+		totalAnimales++;
+		
 	}
-	
+	/*Metodos*/
 	public String movimiento() {
-		String z="desplazarse";
-		return z;
-
+		return("desplazarse");
 	}
-	public static String totalPorTipo() {
-		String x="Mamiferos: 4\n" + "Aves: 4\n" + "Reptiles: 2\n" + "Peces: 2\n" + "Anfibios: 3";
-		return x;
+	public static String totalPorTipo()  {
+		
+		return("Mamiferos: "+ tMamiferos + "\nAves :" + tAves + "\nReptiles :" + tReptiles + "\nPeces :" + tPeces + "\nAnfibios :" + tAnfibios);
 		
 	}
 	@Override
 	public String toString() {
 		if(zona==null) {
-			String a1="Mi nombre es "+this.getNombre()+", tengo una edad de " +this.edad+", habito en "+this.getHabitat()+" y mi genero es "+this.getGenero();
-			return a1;
+			return("Mi nombre es"+ this.getNombre() +", tengo una edad de " + this.getEdad()+", habito en "+ this.getHabitat()+ ", y mi genero es "+ this.getGenero());
 		}
 		else {
-			String a2="Mi nombre es "+this.getNombre()+", tengo una edad de " +this.edad+", habito en "+this.getHabitat()+" y mi genero es "+this.getGenero()+", la zona en la que me ubico es "+this.getZona()+", en el "+this.zona.getZoo();
-			return a2;
+			return("Mi nombre es"+ this.getNombre() +", tengo una edad de " + this.getEdad()+", habito en "+ this.getHabitat()+ ", y mi genero es "+ this.getGenero()+", la zona en la que me ubico es "+this.getZona()+", en el "+this.zona.getZoo());
 		}
+		
 	}
+
+	
+	/*entrada de datos desde otras clases*/
+	public void setnombre(String sn) {
+		this.nombre=sn;
+	}
+	public void setedad(int e) {
+		this.edad=e;
+	}
+	public void sethabitat(String ha) {
+		this.habitat=ha;
+	}
+	public void setgenero(String g) {
+		this.genero=g;
+	}
+	public void setZona(Zona zona) {
+		this.zona = zona;
+	}
+	
+	/*metodos get*/
+	public String getNombre() {
+		return (this.nombre);
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public String getHabitat() {
+		return (this.habitat);
+	}
+	public String getGenero() {
+		return (this.genero);
+	}
+	
+	public Zona getZona() {
+		return zona;
+	}
+
 	public static int getTotalAnimales() {
 		return totalAnimales;
 	}
 	public static void setTotalAnimales(int totalAnimales) {
 		Animal.totalAnimales = totalAnimales;
 	}
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
-	public String getHabitat() {
-		return habitat;
-	}
-	public String getNombre() {
-		return nombre;
-	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public int getEdad() {
-		return edad;
-	}
-	public Zona getZona() {
-		return zona;
-	}
+	
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-
-	public void setZona(Zona zona) {
-		this.zona = zona;
-	}
-	public String getGenero() {
-		return genero;
+	
+	public void setGenero(String genero) {
+		this.genero = genero;
 	}
 	
 	public void setHabitat(String habitat) {
